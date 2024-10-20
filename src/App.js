61b3fd735a8e0f457e2dc7229b61b230c1d48ba4
Roman K <rn1nhf@gmail.com>
2024-10-20 22:19:27 +0300

    lesson 20

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import './App.css'
 
 function App() {
 
-const [product, setProduct] = useState({})
+const [product, setProduct] = useState({
+  ingredients:[]
+})
+
+useEffect(() => {
+  loadProduct()
+}, [])
+
+async function loadProduct(){
+  let res = await fetch('http://localhost:2000/api/product')
+  let resJson = await res.json()
+  setProduct(resJson)
+}
+
+async function onChange(fieldName, value) {
+  let newProduct = {...product}
+  newProduct[fieldName] = value
+  setProduct(newProduct)
+}
 
   return (
-    <>
-   <h1>Less 19 Client (Front-end)</h1>
-   <hr></hr>
-   <button onClick={async ()=>{
-      let res = await fetch('http://localhost:3000/api/product')
-      let resJson = await res.json()
-      console.log(resJson)
-      setProduct(resJson)
-   }}>Load</button>
-   <h2>Блюдо: {product.name}</h2>
-   <h3>{product.description}</h3>
-   </>
+    <div className="max-width-500px flex-direction-column">
+    {/* HEADER */}
+   <div>
+    <input className="widht-100pct font-size-xx-large text-align-center"
+    value={product.name}
+    onChange={(e) => {
+      let value = e.target.value
+      onChange('name', value)   
+    }}/>
+    </div>
+
+   {/* IMAGE */}
+    <div className="flex-direction-column">
+    <img className="widht-100pct" src={product.image}/>
+    <input value={product.image}
+    onChange={(e) => {
+      let value = e.target.value
+      onChange('image', value)   
+    }}/>
+    </div>
+
+   {/* INGREDIENTS */}
+   <div className="flex-direction-column">{
+   product.ingredients.map((ingredient, index) => {
+    return (
+      <input value={ingredient}
+        key={index}
+      onChange={(e) => {
+        let value = e.target.value 
+        let newProduct = {... product}
+        newProduct.ingredients[index] = value
+        setProduct(newProduct)
+      }} />)
+   })
+  }
+   </div>
+
+   {/* DESCRIPTION */}
+    <textarea value={product.description} rows={4}
+      onChange={(e) => {
+      let value = e.target.value
+      onChange('description', value)   
+    }}
+    >
+    </textarea>
+
+   </div>
   );
 }
 
